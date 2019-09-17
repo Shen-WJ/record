a01258645f5582c15bcdf15a57c5bbdf48ef7af3
乔灵 <qiaoling@qiaolingdeMacBook-Pro.local>
2019-09-17 11:36:46 +0800

    增加背景图功能，优化细节

--- src/stores/storeCommon.js
+++ src/stores/storeCommon.js
@@ -2,12 +2,17 @@ import { createStore } from '@mpxjs/core'
 
 export const storeMode = createStore({
   state: {
-    darkMode: false
+    darkMode: false,
+    coverImg: ''
   },
   mutations: {
     openDarkMode (state, { isOpen = false }) {
       wx.setStorageSync("darkMode", isOpen)
       state.darkMode = isOpen
+    },
+    updateCoverImg (state, { imgUrl = '' }) {
+      wx.setStorageSync('coverImg', imgUrl||'')
+      state.coverImg = imgUrl || ''
     }
   }
 })
